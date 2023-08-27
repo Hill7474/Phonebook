6baa6811004afa7e400f2f7170d4058ce53c34b0
Hill7474 <135084696+Hill7474@users.noreply.github.com>
2023-08-26 20:21:33 -0500

    Create Keys.py

---
+++ Keys.py
@@ -0,0 +1,2 @@
+# Phonebook Names and Numbers
+phone_dict = {"name": "Jun", "number": "4173332221", "name": "Ash", "number": "4176464442", "name": "Scott", "number": "4176190000", "name": "Jen", "number": "8168482631"}
